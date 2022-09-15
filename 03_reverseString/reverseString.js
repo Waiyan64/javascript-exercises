ebc7961aca50fa3905cdb54299577118b5925405
waiyan <wphyo8929@gmail.com>
2022-09-15 17:48:18 +0630

    All PASS

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let i = string.length -1;
+    while (i >= 0) {
+        stringreversed += string[i];
+        i--;
+        
+    }
+    let stringreversed2 = stringreversed;
+    stringreversed = '';
+    return stringreversed2;
 
 };
+let stringreversed = '';
 
 // Do not edit below this line
 module.exports = reverseString;
