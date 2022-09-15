05c33796d5a76069ad5b99258e0902d7682f674a
waiyan <wphyo8929@gmail.com>
2022-09-15 17:00:12 +0630

    4 tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    if (number == 0) { 
+        return '';
+    }else {
+        for (let i = 1; i <= number; i++) {
+            stringresult += string;
+    
+        }
+        let stringresult2 = stringresult;
+        stringresult = '';
+        return stringresult2;
+    };    
 };
+let stringresult = '';
 
 // Do not edit below this line
 module.exports = repeatString;
