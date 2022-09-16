41019c39f90db03b2e4796724caf0ecf67406e49
waiyan <wphyo8929@gmail.com>
2022-09-16 22:21:12 +0630

    All PASS

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function() {
+const sumAll = function(value1, value2) {
+    let sum = 0;
+    if (value1 < 0 || value2 < 0) return 'ERROR';
+    else if (!Number.isInteger(value1) || !Number.isInteger(value2)) return 'ERROR';
+    else if (value2 > value1 ) {
+        for (i =  value1; i <= value2; i++) {
+            sum += i;
+        }; return sum;
+    } else if ( value1 > value2 ) {
+        for (i =  value2; i <= value1; i++) {
+            sum += i;
+        }; return sum;
+    } else return "ERROR";
 
 };
 
