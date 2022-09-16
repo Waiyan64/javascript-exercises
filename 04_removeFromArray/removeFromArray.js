1112d4b124ffd9dbaf7a2f78f67e9d9339e1278c
waiyan <wphyo8929@gmail.com>
2022-09-16 14:49:07 +0630

    All PASS

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,12 @@
-const removeFromArray = function(array) {
-    const args = Array.prototype.slice.call(arguments, 1);
-    for(let i = 0; i < array.length; i++) {
-        for(let j = 0; j < args.length; j++) {
-            if(array[i] === args[j]) {
-                array.splice(i, 1);
-            }
-        }
-    }
-    return array;
-
+const removeFromArray = function(...args) {
+    const array = args[0];
+    const newArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        };
+    });
+    return newArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
