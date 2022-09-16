5527966999ecd8a861d6caf566e61d589e9203f9
waiyan <wphyo8929@gmail.com>
2022-09-16 14:40:57 +0630

    Edit javascript

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    const args = Array.prototype.slice.call(arguments, 1);
+    for(let i = 0; i < array.length; i++) {
+        for(let j = 0; j < args.length; j++) {
+            if(array[i] === args[j]) {
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 
 };
 
